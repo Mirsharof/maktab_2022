688da7a66a02c7f60e4539c1cf79b02d32af91db
mirsharof <boss.mirsharof@mail.ru>
2022-06-01 15:14:04 +0300

    git status

--- main.js
+++ main.js
@@ -2,3 +2,19 @@
 window.addEventListener('scroll',()=>{
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY>0);
 })
+
+
+// show answer for question
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq=>{
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open');
+        //change icon
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className==="bx bx-plus"){
+            icon.className= "bx bx-minus";
+        }else{
+            icon.className= "bx bx-plus";
+        }
+    })
+})
